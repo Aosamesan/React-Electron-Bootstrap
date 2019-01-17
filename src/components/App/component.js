a6d93ab17b046ccd8b93d754caf91116fddd8494
Aosamesan <aosamesan@gmail.com>
2019-01-18 03:11:18 +0900

    깔끔하게 다시 정리

--- src/components/App/component.js
+++ src/components/App/component.js
@@ -12,9 +12,8 @@ export default class ViewComponent extends React.Component {
                 <Menu />
                 <div style={{marginTop:"100px"}}>
                     <Switch>
-                        <Redirect exact={true} from="**/index.html" to="/home" /> {/* for Electron */}
-                        <Redirect exact={true} from="/" to="/home" /> {/* for React */}
-                        <Route path="/home" component={Home} />
+                        <Redirect exact={true} from="**/index.html" to="/" /> {/* for Electron */}
+                        <Route exact={true} path="/" component={Home} />
                         <Route path="/hello" component={Hello} />
                         <Route path="/goodbye" component={Goodbye} />
                     </Switch>
--- src/components/Menu/component.js
+++ src/components/Menu/component.js
@@ -13,7 +13,7 @@ export default class ViewComponent extends React.Component {
                 <div className="collapse navbar-collapse" id="main-menu">
                     <ul className="navbar-nav mr-auto">
                         <li className="nav-item">
-                            <NavLink to={'/home'} className="nav-link" activeClassName="active">Home</NavLink>
+                            <NavLink exact={true} to={'/'} className="nav-link" activeClassName="active">Home</NavLink>
                         </li>
                         <li className="nav-item">
                             <NavLink to={'/hello'} className="nav-link" activeClassName="active">Hello</NavLink>
